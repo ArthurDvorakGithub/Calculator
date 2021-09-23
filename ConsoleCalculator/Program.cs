470ac3ad109ee75c6d23d5fadd9b1bf516d9e339
ArthurDvorakGithub <artdvorak@yandex.ru>
2021-09-23 09:16:00 +0300

    Get Priority

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleCalculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите выражение: ");
+                Console.WriteLine(new ExpressionEngine().Calculate(Console.ReadLine()));
+            }
+        }
+    }
+}
